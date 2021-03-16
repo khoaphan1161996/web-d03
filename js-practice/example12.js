f92eec687d06d94cc09017af43e4bf04bfce4ab4
khoaphan1161996 <khoaphan1161996>
2021-03-16 19:11:43 +0700

    frist

--- js-practice/example12.js
+++
@@ -1,7 +0,0 @@
-// Rest
-
-// function sum(fristNum, ...rest) {
-//   return rest.reduce((sum, num) => sum + num, fristNum);
-// }
-
-// console.log(sum(1, 2, 3, 4));
--- js-practice/example5.js
+++
@@ -1,9 +0,0 @@
-// Write a function to insert element at index of array.
-// let array = [21, 4, 5, -1];
-
-// function insert(array, index, element) {
-//   array.splice(index, 0, element);
-//   return array;
-// }
-
-// console.log(insert(array, 3, 99));
