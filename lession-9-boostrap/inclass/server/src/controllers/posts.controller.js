035fd384e55dc7631235eddb668b6ca14f3e5fd2
khoaphan1161996 <khoa071996@gmail.com>
2021-04-08 17:06:04 +0700

    prepare

---
+++ lession-9-boostrap/inclass/server/src/controllers/posts.controller.js
@@ -0,0 +1,158 @@
+const posts = [
+    {
+        id: '1',
+        title: 'rich',
+        content: "i'm rich",
+        author: 'Hieu',
+        time: '1:00',
+        comments: [
+            {author: "khoa",
+             comment: [
+                 "hi","hello"
+             ]}
+        ]
+    },
+    {
+        id: '2',
+        title: 'victory',
+        content: "road to challenge",
+        author: 'Vinh',
+        time: '3:00',
+        comments: [
+            {author: "khoa",
+            comment: [
+                "hi","hello"
+            ]}
+        ]
+    },
+    {
+        id: '3',
+        title: 'poor',
+        content: "i'm poor",
+        author: 'Quang',
+        time: '4:00',
+        comments: [
+            {author: "khoa",
+            comment: [
+                "hi","hello"
+            ]},
+             {author: "vinh",
+             comment: [
+                "bye","hello"
+            ]},
+        ]
+    },
+    {
+        id: '4',
+        title: 'Beauty',
+        content: "i'm beautiful",
+        author: 'Giang',
+        time: '2:45',
+        comments: [
+            {author: "khoa",
+            comment: [
+                "hi","hello"
+            ]},
+             {author: "vinh",
+             comment: [
+                "bye","hello"
+            ]},
+        ]
+    },
+]
+
+module.exports.getPosts = (req,res) => {
+    res.status(200).json({
+        isSuccess: true,
+        message: 'success',
+        posts
+    })  
+}
+
+module.exports.getPostsByID = (req,res) => {
+    const {id} = req.params
+    const post = posts.find(post => post.id === id)
+
+    if(!post){
+        return res.status(400).json({
+            isSuccess: false,
+            message: 'not found post id'
+        })
+    }
+
+    return res.status(200).json({
+        isSuccess: true,
+        message: 'success',
+        post
+    })
+}
+
+module.exports.CreateNewPost = (req,res) => {
+    const {title, content, author} = req.body
+    const now = new Date();
+
+    posts.push({
+        id: `${posts.length + 1}`,
+        title,
+        content,
+        author,
+        time: `${now.getHours()}:${now.getMinutes()}`,
+    })
+
+    res.status(200).json({
+        isSuccess: true,
+        message: 'success',
+        posts
+    })  
+}
+
+module.exports.putPost = (req,res) => {
+    const {id} = req.params
+    const {title, content, author} = req.body
+    
+    const now = new Date();
+    const post = posts.find(post => post.id === id) 
+
+    if(post){
+        let index = posts.indexOf(post)
+        posts[index] = {
+            id: id,
+            title,
+            content,
+            author,
+            time: `${now.getHours()}:${now.getMinutes()}`,
+        }
+        
+        return res.status(200).json({
+            isSuccess: true,
+            message: 'put post id',
+            posts,
+        })
+        }
+        
+        return res.status(400).json({
+            isSuccess: false,
+            message: 'post does not exist',
+        })
+}
+
+module.exports.deletePost = (req,res) => {
+    const {id} = req.params
+    const post = posts.find(post => post.id === id) 
+
+    if(post){
+        let index = posts.indexOf(post)
+        posts.splice(index,1)
+
+        return res.status(200).json({
+            isSuccess: true,
+            message: 'delete post id',
+            posts,
+        })
+    }
+
+    return res.status(400).json({
+        isSuccess: false,
+        message: 'not found the user id',
+    })
+}
