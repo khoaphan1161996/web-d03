9950a7d6f9dd103e698721d2a3a1ab0988690f91
khoa071996@gmail.com <khoaphan1161996>
2021-04-13 20:15:39 +0700

    finishex8

--- lession-8-jquery/homework/server/src/controllers/users.controller.js
+++ lession-8-jquery/homework/server/src/controllers/users.controller.js
@@ -17,7 +17,33 @@ const users = [
     },
 ]
 
-module.exports.deleteUser = (req,res) => {
+module.exports.getUsers = (req, res) => {      
+    res.status(200).json({
+        isSuccess: true,
+        message: 'success',
+        users
+    })     
+  }
+
+module.exports.getUsersByID = (req, res) => {      
+    const {id} = req.params
+    const user = users.find(user => user.id === id)
+
+    if(!user){
+        return res.status(400).json({
+            isSuccess: false,
+            message: 'not find the user id',
+        }) 
+    }
+
+    res.status(200).json({
+        isSuccess: true,
+        message: 'success',
+        user
+    })     
+  }
+
+module.exports.deleteUser = (req, res) => {      
     const {id} = req.params
     const user = users.find(user => user.id === id)
 
@@ -29,16 +55,16 @@ module.exports.deleteUser = (req,res) => {
             isSuccess: true,
             message: 'delete user id',
             users,
-        })
-    }
-    
+        }) 
+    } 
+
     res.status(400).json({
         isSuccess: false,
-        message: 'not found the user id',
-    })
-}
+        message: 'not find the user id',
+    })  
+  }
 
-module.exports.putUser = (req,res) => {
+module.exports.putUser = (req, res) => {      
     const {id} = req.params
     const { name } = req.body
     const user = users.find(user => user.id === id)
@@ -55,10 +81,29 @@ module.exports.putUser = (req,res) => {
             message: 'put user id',
             users,
         })
-    }
+    } 
 
-    res.status(400).json({
+    return res.status(400).json({
         isSuccess: false,
         message: 'user does not exist',
-    })
+  })
 }
+
+module.exports.postUser = (req, res) => {      
+    const { name } = req.body
+
+    if(!name){
+        res.status(400).json({
+            isSuccess: false,
+            message: 'invalid name',
+        })  
+    }
+
+    users.push({id: `${users.length+1}`,name})
+
+    res.status(200).json({
+        isSuccess: true,
+        message: 'success',
+        users
+    })     
+  }
--- lession-8-jquery/homework/server/src/routes/users.route.js
+++ lession-8-jquery/homework/server/src/routes/users.route.js
@@ -3,6 +3,8 @@ const router = express.Router()
 
 const usersController = require('../controllers/users.controller')
 
+router.get('/', usersController.getUsers)
+router.post('/', usersController.postUser) 
 router.delete('/:id', usersController.deleteUser)
 router.put('/:id', usersController.putUser)
 
